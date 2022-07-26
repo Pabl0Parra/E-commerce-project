456734d0c866b6e155321d7452043255c041938b
Pabl0Parra <pabsblue16@gmail.com>
2022-07-26 22:49:21 +0200

    implemented email registration w/email link

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,27 @@
 import { useState } from "react";
+import { auth } from "../../firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   const [email, setEmail] = useState("");
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      url: "https://localhost:3000/register/complete",
+      handleCodeInApp: true,
+    };
+
+    await auth.sendSignInLinkToEmail(email, config);
+    toast.success(
+      `Se ha enviado un email a ${email}. Pinche en el link para completar su registro`
+    );
+    // setting email in localStorage
+    window.localStorage.setItem("EmailForRegistration", email);
+    // clear state
+    setEmail("");
+  };
 
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
@@ -12,10 +30,10 @@ const Register = () => {
         className="form-control"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        autofocus //active field by default
+        autoFocus //active field by default
       />
       <button type="submit" className="btn btn-raised">
-        Register / {email}
+        Register
       </button>
     </form>
   );
@@ -25,6 +43,7 @@ const Register = () => {
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
+          <ToastContainer />
           {registerForm()}
         </div>
       </div>
