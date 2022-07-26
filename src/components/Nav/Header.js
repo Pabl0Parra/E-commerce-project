daa0d1d0f68ae7be1951c1b849bdb4f99ae891ca
Pabl0Parra <pabsblue16@gmail.com>
2022-07-26 21:55:46 +0200

    created navbar

---
+++ src/components/Nav/Header.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import {
+  ShopOutlined,
+  HomeOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+
+const { SubMenu, Item } = Menu;
+
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
+  const handleClick = (e) => {
+    console.log(e.key);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      className="header"
+      onClick={handleClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+    >
+      <Item key="home" icon={<HomeOutlined />}>
+        <Link to="/"> Home</Link>
+      </Item>
+      <Item key="shop" icon={<ShopOutlined />}>
+        <Link to="/shop"> Shop</Link>
+      </Item>
+      <Item key="register" icon={<UserAddOutlined />} className="float-end">
+        <Link to="/register"> Register</Link>
+      </Item>
+      <Item key="login" icon={<UserOutlined />} className="float-end">
+        <Link to="/login"> Login</Link>
+      </Item>
+      {/* <SubMenu icon={<SettingOutlined />} title="Dashboard">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+      </SubMenu> */}
+    </Menu>
+  );
+};
+
+export default Header;
